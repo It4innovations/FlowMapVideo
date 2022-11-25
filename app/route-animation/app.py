71474e2e12bf25bb138c4f2b65f5356089b76ca7
Sofia Michailidu <sofia@michailidu.cz>
2022-11-25 15:57:21 +0100

    added options for title and speed, time label

--- app/route-animation/app.py
+++ app/route-animation/app.py
@@ -12,19 +12,21 @@ from ax_settings import Ax_settings
 from base_graph import get_route_network
 from flowmapviz.collection_plot import plot_routes, WidthStyle
 from df import get_max_vehicle_count, get_max_time, get_min_time
+from math import floor
 
 
-def animate(g, times, ax, ax_settings, timestamp_from, max_count, width_modif, width_style):
+def animate(g, times, ax, ax_settings, timestamp_from, max_count, width_modif, width_style, time_text_artist, speed):
     def step(i):
-        segments = times.loc[timestamp_from + i]
+        timestamp = timestamp_from + i * speed
+        segments = times.loc[timestamp]
 
         ax.clear()
         ax_settings.apply(ax)
         ax.axis('off')
-
+        if(i % 5*60 == 0):
+            time_text_artist.set_text(datetime.utcfromtimestamp(timestamp))
         plot_routes(g, segments, ax=ax,
-                    min_density=1, max_density=10,
-                    min_width_density=10, max_width_density=max_count,
+                    max_width_density=max_count,
                     width_modifier=width_modif,
                     width_style=width_style
                     )
@@ -43,8 +45,10 @@ def animate(g, times, ax, ax_settings, timestamp_from, max_count, width_modif, w
               help="Choose style of width plotting")
 @click.option('--width-modif', default=10, type=click.IntRange(2, 200, clamp=True), show_default=True,
               help="Adjust width.")
+@click.option('--title','-t', default="", help='Set video title')
+@click.option('--speed', default=1, help="Speed up the video.", show_default=True)
 
-def main(data_file, map_file, save_path, frame_start, frames_len, processed_data, save_data, width_style, width_modif):
+def main(data_file, map_file, save_path, frame_start, frames_len, processed_data, save_data, width_style, width_modif, title, speed):
     start = datetime.now()
     g = get_route_network(map_file)
     if processed_data:
@@ -67,12 +71,16 @@ def main(data_file, map_file, save_path, frame_start, frames_len, processed_data
     f, ax_map = plt.subplots()
     fig, ax_map = ox.plot_graph(g, ax=ax_map, show=False, node_size=0)
     fig.set_size_inches(30, 24)
-    ax_density = ax_map.twinx()
-    ax_map_settings = Ax_settings(ylim=ax_map.get_ylim(), aspect=ax_map.get_aspect())
 
     timestamp_from = get_min_time(times_df) + frame_start
     times_len = get_max_time(times_df) - timestamp_from
     times_len = min(int(frames_len), times_len) if frames_len else times_len
+
+    plt.title(title, fontsize=40)
+    time_text = plt.figtext(0.5, 0.09, datetime.utcfromtimestamp(timestamp_from), ha="center", fontsize=25)
+
+    ax_density = ax_map.twinx()
+    ax_map_settings = Ax_settings(ylim=ax_map.get_ylim(), aspect=ax_map.get_aspect())
 
     width_style_enum_option = WidthStyle.EQUIDISTANT
     for el in WidthStyle:
@@ -88,9 +96,11 @@ def main(data_file, map_file, save_path, frame_start, frames_len, processed_data
                                         timestamp_from=timestamp_from,
                                         max_count = max_count,
                                         width_modif = width_modif,
-                                        width_style = width_style_enum_option
+                                        width_style = width_style_enum_option,
+                                        time_text_artist = time_text,
+                                        speed = speed
                                         ),
-                                    interval=150, frames=times_len, repeat=False)
+                                    interval=75, frames=floor(times_len/speed), repeat=False)
     timestamp = round(time() * 1000)
     anim.save(path.join(save_path, str(timestamp) + "-rt.mp4"), writer="ffmpeg")
 
