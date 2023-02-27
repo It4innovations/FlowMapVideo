a9315cef612522f71b6244fe42a4c2aa2db77ecf
Martin Šurkovský <martin.surkovsky@vsb.cz>
2023-02-27 14:25:48 +0100

    WIP: refactoring of input.py n# Please enter the commit message for your changes. Lines starting

--- flowmap/input.py
+++ flowmap/input.py
@@ -1,38 +1,28 @@
-from dataclasses import dataclass
+from collections import defaultdict
+from dataclasses import dataclass, InitVar, asdict
 from datetime import datetime, timedelta
 import operator
 from itertools import zip_longest
 from pandas import date_range, DateOffset, to_datetime
-from numpy import linspace, int64, where
+import numpy as np
 from math import ceil
 from operator import attrgetter
 from itertools import groupby
 import gc
 import numpy as np
-
-graph = None
+from networkx import MultiDiGraph as Graph
 
 @dataclass
 class Density:
+    """Segment in time with count of vehicles on left and right side of the segment."""
+
+    # TODO: find out a better name!
+    #
     timestamp: int
     node_from: int
     node_to: int
     count_from: int
     count_to: int
-
-    def __init__(
-        self,
-        timestamp: int,
-        node_from: int,
-        node_to: int,
-        count_from: int,
-        count_to: int
-        ):
-        self.timestamp = timestamp
-        self.node_to = node_to
-        self.node_from = node_from
-        self.count_from = count_from
-        self.count_to = count_to
 
 
 @dataclass
@@ -45,184 +35,106 @@ class Row: # (Record)
     status: str
     node_from: int
     node_to: int
-    length: float
-    count_from: int
-    count_to: int
+    count_from: int = 0
+    count_to: int = 0
+    length: InitVar[float] = None
+    graph: InitVar[Graph] = None
 
-    def __init__(
-        self,
-        vehicle_id: int,
-        segment_id: str,
-        start_offset_m: float,
-        speed_mps: float,
-        status: str,
-        node_from: int,
-        node_to: int,
-        interval: int,
-        length: float = None,
-        timestamp: int = None
-        ):
-        global graph
-        self.timestamp = timestamp
-        self.vehicle_id = vehicle_id
-        self.segment_id = segment_id
-        self.start_offset_m = start_offset_m
-        self.speed_mps = speed_mps
-        self.status = status
-        self.node_to = node_to
-        self.node_from = node_from
+    def __post_init__(self, length, graph):
+        self.length = length;
 
-        if length is not None:
-            self.length = length
-        else:
-            if node_from is None or node_to is None:
-                self.length = None
-            else:
-                data = graph.get_edge_data(node_from, node_to)
-                # NOTE: uncomment assert, comment returning nan
-                # assert data
-                if data is not None:
-                    data = data[0]
-                    assert "length" in data
-                    self.length = data['length']
-                else:
-                    self.length = None
+        if length is None and (self.node_from is not None or self.node_to is not None):
+            data = graph.get_edge_data(self.node_from, self.node_to)
+            assert data is not None
 
-        self.count_from = 0
-        self.count_to = 0
+            data = data[0]
+            assert "length" in data
+            self.length = data['length']
+
+    @staticmethod
+    def create_from_existing(timestamp, start_offset_m, base_record):
+        params = asdict(base_record)
+        params['timestamp'] = timestamp
+        params['start_offset_m'] = start_offset_m
+
+        return Row(**params)
 
 
-def add_count_to_dictionary(dictionary, key, lock=None):
-    if lock is not None:
-        with lock:
-            if key not in dictionary:
-                dictionary[key] = 1
-            else:
-                dictionary[key] += 1
-    else:
-        if key not in dictionary:
-            dictionary[key] = 1
-        else:
-            dictionary[key] += 1
 
-def preprocess_fill_missing_times(df, g, speed=1, fps=25, counts_dict = {}, lock = None):
-    global graph
-    graph = g
+def preprocess_fill_missing_times(df, graph, speed=1, fps=25):
+
+    node_counts = defaultdict(lambda: 0)
 
     interval = speed / fps
 
     start = datetime.now()
 
     # change datetime to int
-    df['timestamp']= to_datetime(df['timestamp']).astype(int64)//10**6 # resolution in milliseconds
+    df['timestamp']= to_datetime(df['timestamp']).astype(np.int64)//10**6 # resolution in milliseconds
 
-    df['timestamp'] = df['timestamp'].div(1000 * interval).round().astype(int64)
+    df['timestamp'] = df['timestamp'].div(1000 * interval).round().astype(np.int64)
     df = df.groupby(['timestamp','vehicle_id']).first().reset_index()
 
     rows = [Row(interval=interval, **kwargs) for kwargs in df.to_dict(orient='records')]
-    del df
-    gc.collect()
-
     rows = sorted(rows, key=lambda x: (x.vehicle_id, x.timestamp))
-    rows = list(zip_longest(rows, rows[1:], fillvalue=Row(None,None,None,None,None,None,None,None,None)))
 
-    rows_new = []
-
-    for i, row in enumerate(rows):
-        if row[1].timestamp is None:
-            rows_new.append(row[0])
-            if(row[0].start_offset_m < row[0].length/2):
-                closer_node = row[0].node_from
-                row[0].count_from = 1
+    new_rows = []
+    for i, (row, next_row) in enumerate(zip(rows[:], rows[1:] + [None])):  # TODO: row => record; processing_record & next_record
+        if next_row is None or row.vehicle_id != next_row.vehicle_id:
+            new_rows.append(row)
+            if(row.start_offset_m < row.length / 2):
+                closer_node = row.node_from
+                row.count_from = 1
             else:
-                closer_node =  row[0].node_to
-                row[0].count_to = 1
+                closer_node =  row.node_to
+                row.count_to = 1
 
-            add_count_to_dictionary(counts_dict, (row[0].timestamp, closer_node), lock)
-            continue
-
-        if row[0].length is None or row[1].length is None:
-            continue
-
-        if row[1].timestamp is None or row[0].vehicle_id != row[1].vehicle_id:
-            rows_new.append(row[0])
-            if(row[0].start_offset_m < row[0].length/2):
-                closer_node = row[0].node_from
-                row[0].count_from = 1
-            else:
-                closer_node =  row[0].node_to
-                row[0].count_to = 1
-
-            add_count_to_dictionary(counts_dict, (row[0].timestamp, closer_node), lock)
-
-        else:
-            new_timestamps = [*range(row[0].timestamp, row[1].timestamp)][:-1]
+            node_counts[(row.timestamp, closer_node)] += 1
+        else:  # fill missing records
+            new_timestamps = [*range(row.timestamp, next_row.timestamp)][:-1]
             new_params = []
 
-            if (row[0].segment_id == row[1].segment_id):
-                new_offsets = linspace(row[0].start_offset_m, row[1].start_offset_m,len(new_timestamps) + 1)[:-1]
-                new_params = zip(new_timestamps, new_offsets)
-                is_first_segment = None
+            if row.segment_id == next_row.segment_id:
+                new_offsets = np.linspace(row.start_offset_m, next_row.start_offset_m, len(new_timestamps), endpoint=False)
+                processing_segments = [row] * len(new_offsets)
             else:
-                new_offsets = linspace(row[0].start_offset_m, row[1].start_offset_m + row[0].length,len(new_timestamps) + 1)[:-1]
-                is_first_segment = where(new_offsets<row[0].length, True, False)
-                new_offsets = where(new_offsets>row[0].length, new_offsets - row[0].length, new_offsets)
-                new_params = zip(new_timestamps, new_offsets)
+                # 1. fill missing offset between two consecutive records
+                new_offsets = np.linspace(row.start_offset_m, next_row.start_offset_m + row.length, len(new_timestamps), endpoint=False)
+                # 2. adjust offset where the segment change
+                new_offsets = np.where(new_offsets > row.length, new_offsets - row.length, new_offsets)
+                processing_segments = np.where(new_offsets < row.length, row, next_row)
+
+            new_params = zip(new_timestamps, new_offsets)
 
             closer_node = None
-            half_length_first = row[0].length / 2
-            half_length_second = row[1].length / 2
-            for i, param in enumerate(new_params):
-                if(is_first_segment is None or is_first_segment[i]):
-                    row_new = Row(
-                        timestamp = param[0],
-                        segment_id = row[0].segment_id,
-                        start_offset_m = param[1],
-                        speed_mps = row[0].speed_mps,
-                        status = row[0].status,
-                        node_from = row[0].node_from,
-                        node_to = row[0].node_to,
-                        vehicle_id = row[0].vehicle_id,
-                        length = row[0].length,
-                        interval=interval
-                        )
-                    rows_new.append(row_new)
+            half_length_current = row.length / 2
+            half_length_next = next_row.length / 2
+            for segment, (timestamp, start_offset_m) in zip(processing_segments, new_params):
+                    timestamp, start_offset_m = param
+                    new_row = Row.create_from_existing(timestamp, start_offset_m, segment)
+                    new_rows.append(row_new)
 
-                    if(param[1] < half_length_first):
-                        closer_node = row[0].node_from
-                        row_new.count_from = 1
-
+                    if start_offset_m < half_length_first: # TODO: think of finer division
+                        closer_node = row.node_from
+                        # row_new.count_from = 1  # TODO: rename to 'attach_to' wihich is a number from division paramter
                     else:
-                        closer_node = row[0].node_to
-                        row_new.count_to = 1
+                        closer_node = row.node_to
+                    #     row_new.count_to = 1
+                    new_row.attach_to = None # TODO: compute based on division number
+                # else:
+                #     new_row = Row.create_from_existing(timestamp, start_offset_m, next_row)
+                #     rows_new.append(row_new)
 
-                else:
-                    row_new = Row(
-                        timestamp = param[0],
-                        segment_id = row[1].segment_id,
-                        start_offset_m = param[1],
-                        speed_mps = row[1].speed_mps,
-                        status = row[1].status,
-                        node_from = row[1].node_from,
-                        node_to = row[1].node_to,
-                        vehicle_id = row[1].vehicle_id,
-                        length = row[1].length,
-                        interval=interval
-                        )
-                    rows_new.append(row_new)
+                #     if start_offset_m < half_length_second:  # TODO: think of finer division
+                #         closer_node = new_rows[-1].node_from
+                #         row_new.count_from = 1
+                #     else:
+                #         closer_node =  new_rows[-1].node_to
+                #         row_new.count_to = 1
 
-                    if(param[1] < half_length_second):
-                        closer_node = rows_new[-1].node_from
-                        row_new.count_from = 1
-                    else:
-                        closer_node =  rows_new[-1].node_to
-                        row_new.count_to = 1
+                counts_dict[(new_rows[-1].timestamp, closer_node)] += 1
 
-                add_count_to_dictionary(counts_dict, (rows_new[-1].timestamp, closer_node), lock)
-
-    del rows
-    gc.collect()
-    return rows_new, counts_dict
+    return new_rows, counts_dict
 
 
 def preprocess_add_counts(records, counts_dict):
