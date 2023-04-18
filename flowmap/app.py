f65385903bfd368db83d374a2a1c9a5e9d5a63cf
Sofia Michailidu <sofia@michailidu.cz>
2023-04-18 15:37:41 +0200

    code formatting and variable renaming

--- flowmap/app.py
+++ flowmap/app.py
@@ -27,16 +27,27 @@ def cli():
     pass
 
 
-def animate(g, t_seg_dict, ax, ax_settings, timestamp_from, max_count, width_modif, width_style, time_text_artist, interval):
+def animate(
+    g,
+    timed_seg_dict,
+    ax,
+    ax_settings,
+    timestamp_from,
+    max_count,
+    width_modif,
+    width_style,
+    time_text_artist,
+    interval
+):
     def step(i):
         ax.clear()
         ax_settings.apply(ax)
         ax.axis('off')
 
-        timestamp = timestamp_from + i # * speed # * round(1000 / fps)
-        if(i % 5*60 == 0):
-            time_text_artist.set_text(datetime.utcfromtimestamp(timestamp * 1000 * interval //10**3))
-        segments = t_seg_dict[timestamp]
+        timestamp = timestamp_from + i
+        if i % 5 * 60 == 0:
+            time_text_artist.set_text(datetime.utcfromtimestamp(timestamp * 1000 * interval // 10**3))
+        segments = timed_seg_dict[timestamp]
         nodes_from = [seg.node_from.id for seg in segments]
         nodes_to = [seg.node_to.id for seg in segments]
         densities = [seg.counts for seg in segments]
@@ -48,26 +59,56 @@ def animate(g, t_seg_dict, ax, ax_settings, timestamp_from, max_count, width_mod
             densities=densities,
             max_width_density=max_count,
             width_modifier=width_modif,
-            width_style=width_style)
+            width_style=width_style
+        )
+
     return step
 
 
 @cli.command()
 @click.argument('simulation_path')
 @click.option('--fps', '-f', default=25, help="Set video frames per second.", show_default=True)
-@click.option('--save-path', default="", help='Path to the folder for the output video.')
+@click.option('--save-path', default='', help="Path to the folder for the output video.")
 @click.option('--frame-start', default=0, help="Number of frames to skip before plotting.")
 @click.option('--frames-len', help="Number of frames to plot")
-@click.option('--processed-data','-p', is_flag=True, help="Data is already processed")
-@click.option('--save-data','-s', is_flag=True, help="Save processed data")
-@click.option('--width-style', type=click.Choice([el.name for el in WidthStyle]), default='BOXED',
-              help="Choose style of width plotting")
-@click.option('--width-modif', default=10, type=click.IntRange(2, 200, clamp=True), show_default=True,
-              help="Adjust width.")
-@click.option('--title','-t', default="", help='Set video title')
+@click.option('--processed-data', '-p', is_flag=True, help="Data is already processed")
+@click.option('--save-data', '-s', is_flag=True, help="Save processed data")
+@click.option(
+    '--width-style',
+    type=click.Choice([el.name for el in WidthStyle]),
+    default='BOXED',
+    help="Choose style of width plotting"
+)
+@click.option(
+    '--width-modif',
+    default=10,
+    type=click.IntRange(2, 200, clamp=True),
+    show_default=True,
+    help="Adjust width."
+)
+@click.option('--title', '-t', default='', help="Set video title")
 @click.option('--speed', default=1, help="Speed up the video.", show_default=True)
-@click.option('--divide', '-d', default=2, help="Into how many parts will each segment be split.", show_default=True)
-def generate_animation(simulation_path, fps, save_path, frame_start, frames_len, processed_data, save_data, width_style, width_modif, title, speed, divide):
+@click.option(
+    '--divide',
+    '-d',
+    default=2,
+    help="Into how many parts will each segment be split.",
+    show_default=True
+)
+def generate_animation(
+    simulation_path,
+    fps,
+    save_path,
+    frame_start,
+    frames_len,
+    processed_data,
+    save_data,
+    width_style,
+    width_modif,
+    title,
+    speed,
+    divide
+):
     mpl.use('Agg')
     ts = TimerSet()
     with ts.get("data loading"):
@@ -77,61 +118,70 @@ def generate_animation(simulation_path, fps, save_path, frame_start, frames_len,
 
     with ts.get("data preprocessing"):
         if not processed_data:
-            t_segments = fill_missing_times(sim_history, g, speed, fps, divide)
+            timed_segments = fill_missing_times(sim_history, g, speed, fps, divide)
 
         if save_data:
             with open('preprocessed_data.pickle', 'wb') as h:
-                pickle.dump(t_segments, h)
+                pickle.dump(timed_segments, h)
 
-    with ts.get("preparing for animation"):
-        timestamps = [seg.timestamp for seg in t_segments]
+        timestamps = [seg.timestamp for seg in timed_segments]
         min_timestamp = min(timestamps)
         max_timestamp = max(timestamps)
 
-        max_count = max([max(seg.counts) for seg in t_segments])
+        max_count = max([max(seg.counts) for seg in timed_segments])
 
         timestamp_from = min_timestamp + frame_start
-        times_len = max_timestamp - timestamp_from
-        times_len = min(int(frames_len), times_len) if frames_len else times_len
+        num_of_frames = max_timestamp - timestamp_from
+        num_of_frames = min(int(frames_len), num_of_frames) if frames_len else num_of_frames
 
-        t_seg_dict = defaultdict(list)
-        for seg in t_segments:
-            t_seg_dict[seg.timestamp].append(seg)
+        timed_seg_dict = defaultdict(list)
+        for seg in timed_segments:
+            timed_seg_dict[seg.timestamp].append(seg)
 
     with ts.get("map preparing"):
         width_style = WidthStyle[width_style]
 
         fig, ax_map = plt.subplots()
-        plot_graph_with_zoom(g, ax_map, secondary_sizes = [1, 0.7, 0.5, 0.3])
+        plot_graph_with_zoom(g, ax_map, secondary_sizes=[1, 0.7, 0.5, 0.3])
         fig.set_size_inches(30, 24)
 
         plt.title(title, fontsize=40)
-        time_text = plt.figtext(0.5, 0.09, datetime.utcfromtimestamp(timestamp_from//10**3), ha="center", fontsize=25)
+        time_text = plt.figtext(
+            0.5,
+            0.09,
+            datetime.utcfromtimestamp(timestamp_from // 10**3),
+            ha='center',
+            fontsize=25
+        )
 
         ax_density = ax_map.twinx()
         ax_map_settings = Ax_settings(ylim=ax_map.get_ylim(), aspect=ax_map.get_aspect())
 
     with ts.get("create animation"):
-        anim = animation.FuncAnimation(plt.gcf(),
-                                        animate(
-                                            g,
-                                            t_seg_dict,
-                                            ax_settings=ax_map_settings,
-                                            ax=ax_density,
-                                            timestamp_from=timestamp_from,
-                                            max_count = max_count,
-                                            width_modif = width_modif,
-                                            width_style = width_style,
-                                            time_text_artist = time_text,
-                                            interval = speed / fps
-                                            ),
-                                        interval=75, frames=floor(times_len), repeat=False)
+        anim = animation.FuncAnimation(
+            plt.gcf(),
+            animate(
+                g,
+                timed_seg_dict,
+                ax_settings=ax_map_settings,
+                ax=ax_density,
+                timestamp_from=timestamp_from,
+                max_count=max_count,
+                width_modif=width_modif,
+                width_style=width_style,
+                time_text_artist=time_text,
+                interval=speed / fps
+            ),
+            interval=75,
+            frames=floor(num_of_frames),
+            repeat=False
+        )
 
         timestamp = round(time() * 1000)
-        anim.save(path.join(save_path, str(timestamp) + "-rt.mp4"), writer="ffmpeg", fps=fps)
+        anim.save(path.join(save_path, str(timestamp) + '-rt.mp4'), writer='ffmpeg', fps=fps)
 
-    for k,v in ts.collect().items():
-        print(f"{k}: {v} ms")
+    for k, v in ts.collect().items():
+        print(f'{k}: {v} ms')
 
 
 class TimeUnit(Enum):
@@ -141,21 +191,24 @@ class TimeUnit(Enum):
 
     @staticmethod
     def from_str(name):
-        if name == "seconds":
+        if name == 'seconds':
             return TimeUnit.SECONDS
-        elif name == "minutes":
+        elif name == 'minutes':
             return TimeUnit.MINUTES
-        elif name == "hours":
+        elif name == 'hours':
             return TimeUnit.HOURS
 
         raise Exception(f"Invalid time unit: '{name}'.")
 
 
 @cli.command()
-@click.argument("simulation-path", type=click.Path(exists=True))
-@click.option("--time-unit", type=str,
-              help="Time unit. Possible values: [seconds|minutes|hours]",
-              default="hours")
+@click.argument('simulation-path', type=click.Path(exists=True))
+@click.option(
+    '--time-unit',
+    type=str,
+    help="Time unit. Possible values: [seconds|minutes|hours]",
+    default='hours'
+)
 @click.option('--speed', default=1, help="Speed up the video.", show_default=True)
 def get_info(simulation_path, time_unit, speed):
 
@@ -166,11 +219,13 @@ def get_info(simulation_path, time_unit, speed):
 
     real_time = (sim.history.data[-1][0] - sim.history.data[0][0]) / time_unit.value
 
-    print (f"Real time duration: {real_time} {time_unit.name.lower()}.")
+    print(f'Real time duration: {real_time} {time_unit.name.lower()}.')
 
-    real_time_minutes = (sim.history.data[-1][0] - sim.history.data[0][0]) / time_unit_minutes.value
+    real_time_minutes = (
+        sim.history.data[-1][0] - sim.history.data[0][0]
+    ) / time_unit_minutes.value
 
-    print (f"Video length: {real_time_minutes / speed} minutes.")
+    print(f'Video length: {real_time_minutes / speed} minutes.')
 
 
 def main():
--- flowmap/input.py
+++ flowmap/input.py
@@ -9,7 +9,7 @@ from typing import NewType
 from ruth.metaclasses import Singleton
 
 
-NodeId = NewType("NodeId", int)
+NodeId = NewType('NodeId', int)
 
 
 @dataclass
@@ -44,7 +44,7 @@ class SegmentInTime(metaclass=Singleton):
         self.divide = divide
 
     def __hash__(self):
-      return hash((self.node_from.id, self.node_to.id, self.timestamp))
+        return hash((self.node_from.id, self.node_to.id, self.timestamp))
 
     def add_vehicle(self, start_offset_m):
         self.offsets.append(start_offset_m)
@@ -64,7 +64,11 @@ class SegmentInTime(metaclass=Singleton):
 
     @property
     def counts(self):
-        return [self.node_from.vehicle_count, *self.inner_counts, self.node_to.vehicle_count]
+        return [
+            self.node_from.vehicle_count,
+            *self.inner_counts,
+            self.node_to.vehicle_count
+        ]
 
 
 @dataclass
@@ -81,44 +85,48 @@ class Record:
     length: InitVar[float] = None
     graph: InitVar[Graph] = None
 
-    def __post_init__(self, length, graph = None):
+    def __post_init__(self, length, graph=None):
         self.length = length
 
-        if length is None and graph is not None and (self.node_from is not None or self.node_to is not None):
+        if (
+            length is None
+            and graph is not None
+            and (self.node_from is not None or self.node_to is not None)
+        ):
             data = graph.get_edge_data(self.node_from, self.node_to)
             assert data is not None
 
             data = data[0]
-            assert "length" in data
+            assert 'length' in data
             self.length = data['length']
 
 
-def add_vehicle(record, divide: int, timestamp = None, start_offset_m = None):
+def add_vehicle(record, divide: int, timestamp=None, start_offset_m=None):
     """Add vehicle to **singleton** segment in time element."""
     if timestamp is None:
         timestamp = record.timestamp
     if start_offset_m is None:
         start_offset_m = record.start_offset_m
 
-    t_seg = SegmentInTime(record.node_from,
-                            record.node_to,
-                            record.length,
-                            timestamp,
-                            divide)
-    t_seg.add_vehicle(start_offset_m)
+    timed_seg = SegmentInTime(
+        record.node_from, record.node_to, record.length, timestamp, divide
+    )
+    timed_seg.add_vehicle(start_offset_m)
 
-    return t_seg
+    return timed_seg
 
 
-def fill_missing_times(df, graph, speed=1, fps=25, divide: int=2):
-    assert divide >= 2, f"Invalid value of divide '{divide}'. It must be greater or equal to 2."
+def fill_missing_times(df, graph, speed=1, fps=25, divide: int = 2):
+    assert (
+        divide >= 2
+    ), f"Invalid value of divide '{divide}'. It must be greater or equal to 2."
 
-    t_segments = set()
+    timed_segments = set()
 
     interval = speed / fps
 
     # change datetime to int
-    df['timestamp']= to_datetime(df['timestamp']).astype(np.int64)//10**6 # resolution in milliseconds
+    df['timestamp'] = to_datetime(df['timestamp']).astype(np.int64) // 10**6 # resolution in milliseconds
 
     df['timestamp'] = df['timestamp'].div(1000 * interval).round().astype(np.int64)
     df = df.groupby(['timestamp','vehicle_id']).first().reset_index()
@@ -126,27 +134,49 @@ def fill_missing_times(df, graph, speed=1, fps=25, divide: int=2):
     records = [Record(**kwargs, graph=graph) for kwargs in df.to_dict(orient='records')]
     records = sorted(records, key=lambda x: (x.vehicle_id, x.timestamp))
 
-    for i, (processing_record, next_record) in enumerate(zip(records[:], records[1:] + [None])):
-        if next_record is None or processing_record.vehicle_id != next_record.vehicle_id:
-
-            t_segments.add(add_vehicle(processing_record, divide))
+    for i, (processing_record, next_record) in enumerate(
+        zip(records[:], records[1:] + [None])
+    ):
+        if (
+            next_record is None
+            or processing_record.vehicle_id != next_record.vehicle_id
+        ):
+            timed_segments.add(add_vehicle(processing_record, divide))
         else:  # fill missing records
             new_timestamps = [*range(processing_record.timestamp, next_record.timestamp)]
             new_params = []
 
             if processing_record.segment_id == next_record.segment_id:
-                new_offsets = np.linspace(processing_record.start_offset_m, next_record.start_offset_m, len(new_timestamps), endpoint=False)
+                new_offsets = np.linspace(
+                    processing_record.start_offset_m,
+                    next_record.start_offset_m,
+                    len(new_timestamps),
+                    endpoint=False
+                )
                 processing_segments = [processing_record] * len(new_offsets)
             else:
                 # 1. fill missing offset between two consecutive records
-                new_offsets = np.linspace(processing_record.start_offset_m, next_record.start_offset_m + processing_record.length, len(new_timestamps), endpoint=False)
+                new_offsets = np.linspace(
+                    processing_record.start_offset_m,
+                    next_record.start_offset_m + processing_record.length,
+                    len(new_timestamps),
+                    endpoint=False
+                )
                 # 2. adjust offset where the segment change
-                processing_segments = np.where(new_offsets > processing_record.length, next_record, processing_record)
-                new_offsets = np.where(new_offsets > processing_record.length, new_offsets - processing_record.length, new_offsets)
+                processing_segments = np.where(
+                    new_offsets > processing_record.length,
+                    next_record,
+                    processing_record
+                )
+                new_offsets = np.where(
+                    new_offsets > processing_record.length,
+                    new_offsets - processing_record.length,
+                    new_offsets
+                )
 
             new_params = zip(new_timestamps, new_offsets)
 
             for record, (timestamp, start_offset_m) in zip(processing_segments, new_params):
-                t_segments.add(add_vehicle(record, divide, timestamp, start_offset_m))
+                timed_segments.add(add_vehicle(record, divide, timestamp, start_offset_m))
 
-    return t_segments
+    return timed_segments
