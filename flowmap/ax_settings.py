c6e4747447a082e5f56ae77f629b2a73ea6f4b49
Sofia Michailidu <sofia@michailidu.cz>
2023-04-24 14:30:59 +0200

    remove unnecessary function: twin_axes

--- flowmap/ax_settings.py
+++ flowmap/ax_settings.py
@@ -1,5 +1,4 @@
 from matplotlib import pyplot as plt
-import osmnx as ox
 
 
 class Ax_settings:
@@ -10,13 +9,3 @@ class Ax_settings:
     def apply(self, ax):
         ax.set_ylim(self.ylim)
         ax.set_aspect(self.aspect)
-
-
-def twin_axes(g):
-    f, ax_map = plt.subplots()
-    _, ax_map = ox.plot_graph(g, ax=ax_map, node_size=0, show=False)
-    ax_density = ax_map.twinx()
-    ax_density.axis('off')
-    ax_map_settings = Ax_settings(ylim=ax_map.get_ylim(), aspect=ax_map.get_aspect())
-    ax_map_settings.apply(ax_density)
-    return f, ax_density, ax_map_settings
